86683cfd5727d501fcf093c06fa474ac0ddd892c
asadprab <asadprabowo729@gmail.com>
2025-01-10 23:50:28 +0700

    Add files via upload

--- config/database.php
+++ config/database.php
@@ -1,1 +1,13 @@
+<?php
+$hostname = "localhost";
+$username = "root"; // Sesuaikan dengan pengaturan XAMPP
+$password = ""; // Kosongkan jika default XAMPP
+$database = "zadmin_desa"; // Ganti dengan nama database Anda
 
+// Membuat koneksi dengan MySQLi
+$conn = mysqli_connect($hostname, $username, $password, $database);
+
+// Cek koneksi
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
